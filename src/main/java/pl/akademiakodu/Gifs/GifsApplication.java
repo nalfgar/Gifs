88ed53bdc309299fe0449d6e0a8a73f2182c2fe3
Darek Strojecki <dstroj@wp.pl>
2017-11-16 16:44:59 +0100

    First Commit

---
+++ src/main/java/pl/akademiakodu/Gifs/GifsApplication.java
@@ -0,0 +1,12 @@
+package pl.akademiakodu.Gifs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GifsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GifsApplication.class, args);
+	}
+}
